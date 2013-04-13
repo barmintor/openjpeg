e8d2140c68c090f36e954cbdc24ab8b0dde37cb7
Kevin S. Clarke <ksclarke@gmail.com>
2013-04-13 01:59:03 -0400

    fixed test paths and included openjpeg test data

--- wrapping/jni/openjp2/tests/info/freelibrary/openjpeg/JP2BridgeTest.java
+++ wrapping/jni/openjp2/tests/info/freelibrary/openjpeg/JP2BridgeTest.java
@@ -16,9 +16,14 @@ public class JP2BridgeTest {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(JP2BridgeTest.class);
 
+	private static final String TIF_DIR = "target/openjpeg-data--master/baseline/conformance/";
+	private static final String OUTPUT_DIR = "target/test-resources";
+	
 	@BeforeClass
 	public static void setUp() throws Exception {
 		File logDir = new File("target/logs");
+
+		// A workaround that lets us create a directory for hs_err_pid logs
 		if (!logDir.exists()) {
 			if (!logDir.mkdirs()) {
 				throw new RuntimeException("Couldn't create target/logs dir");
@@ -48,39 +53,43 @@ public class JP2BridgeTest {
 		JP2Bridge jp2Bridge = new JP2Bridge();
 
 		try {
-			File input = new File("/home/kevin/testimage.ppm");
-			File output = new File("/tmp/testimage.jp2");
+// need to configure TIF libs to be accessible
+//			File input = new File(TIF_DIR + "jp2_2.tif");
+//			File output = new File(OUTPUT_DIR + "jp2_2.jp2");
 
+			File input = new File(TIF_DIR + "c0p0_01.pgx");
+			File output = new File(OUTPUT_DIR + "c0p0_01.jp2");
+			
 			jp2Bridge.encode(jp2Bridge.getEncoding(input, output));
 		}
 		catch (Exception details) {
 			if (LOGGER.isDebugEnabled()) {
 				LOGGER.debug(stringifyTrace(details));
 			}
-			
+
 			fail(details.getMessage());
 		}
 	}
-	
+
 	private String stringifyTrace(Exception aException) {
 		StringBuilder trace = new StringBuilder(aException.getMessage() + EOL);
-		
+
 		for (StackTraceElement details : aException.getStackTrace()) {
 			StringBuilder message = new StringBuilder("   ");
 			int lineNumber = details.getLineNumber();
-			
+
 			message.append(details.getClassName());
-			
+
 			if (lineNumber > 0) {
 				message.append(" (").append(lineNumber).append(") ");
 			}
 			else if (details.isNativeMethod()) {
 				message.append(" (native) ");
 			}
-			
+
 			trace.append(message.append(EOL).toString());
 		}
-		
+
 		return trace.toString();
 	}
 
